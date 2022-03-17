3cc69b8934369da942851637072263e9d3b2a28a
vektor0 <99178713+vektor0@users.noreply.github.com>
2022-03-17 15:24:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,41 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value <=1)
+    return false;
+  for (int z = 2; z <= (value/2); z++) {
+    if ((value % z) == 0)
+      return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t c = 0;
+  uint64_t z = 0;
+  while (z != n) {
+    c++;
+    if (checkPrime(c))
+      z++;
+  }
+  return c;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  value++;
+    while (checkPrime(value) != true) {
+      value++;
+    }
+    return value;
 }
 
+
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t ss = 0;
+    for (uint64_t z = 2; z < hbound; z++) {
+      if (checkPrime(z) == true)
+        ss += z;
+    }
+    return ss;
 }
